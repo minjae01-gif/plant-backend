bc40b18031cfa17da7ee9214a5f33f5a73ef4476
minjae01-gif <alswodmlgo@gmail.com>
2026-05-20 09:41:23 +0900

    db타임아웃 추가

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,9 +8,13 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+
+  connectTimeout: 10000,
+
   ssl: {
     rejectUnauthorized: false
   }
